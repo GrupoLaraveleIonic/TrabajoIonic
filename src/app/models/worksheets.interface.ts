14b34a51ed75b6c597b86e28c79359e52e583cf7
RafaelJaimeMoreno <rajaimor@gmail.com>
2020-11-25 15:58:15 +0100

    Commit con stashed y cosas raras

--- src/app/models/worksheets.interface.ts
+++ src/app/models/worksheets.interface.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export interface worksheet {
     id?:string;
     date:Date;
     description:string;
     useruid:string;
 }
-=======
-
-export interface worksheet{
-    id?: string;
-    date: Date;
-    description: string;
-}
->>>>>>> 06ece93bdc6c08827d9dd8d0572cc46811fa547b
